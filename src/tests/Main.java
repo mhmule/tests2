093716873a1b81ae3f6f921fb50b633be4aaa848
Miloslav Hantl <miloslav.hantl@gmail.com>
2017-04-23 10:28:53 +0200

    init

---
+++ src/tests/Main.java
@@ -0,0 +1,15 @@
+package tests;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Sales sales = new Sales();
+		sales.display();
+		displayGreeting();
+	}
+
+	private static void displayGreeting() {
+		System.out.println("HELLO happy Sales people!!!");
+		System.out.println("This is show application proj TESTS!");
+	}
+}
---
+++ src/tests/Sales.java
@@ -0,0 +1,8 @@
+package tests;
+
+public class Sales {
+
+	public void display() {
+		System.out.println("Git2 test ... some data from Sales class");
+	}
+}
